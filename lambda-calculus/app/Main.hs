3e89473ccb67329d833ad3264de580004663c8a7
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-24 21:00:49 +0300

    lambda-calculus/features: basis

--- lambda-calculus/app/Main.hs
+++ lambda-calculus/app/Main.hs
@@ -4,7 +4,25 @@ module Main where
 
 mul = \a b -> a * b
 
-identity = \x -> x
+identity' = \x -> x
+
+
+--SKI basis
+identity x = x -- I combinator
+constant x y = x -- K combinator
+-- s x y z = x z (y z)
+s f g x = f x (g x) -- S combinator
+--s f g x = f x $ g x -- S combinator
+
+apply f x = f x
+
+
+-- f . g => f(g(x))
+-- var compose = f => g => x => f(g(x))
+--compose f g = \x -> f(g x)
+compose f g x = f (g x)
+
+
 
 main :: IO ()
 main = putStrLn "Hello, world"
